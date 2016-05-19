2687ece7c9562f5a6ac7a6476cf5acd00440f117
Aula Astur <AulaAstur@users.noreply.github.com>
2016-05-19 17:46:33 +0200

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,13 @@
+package almacen;
+import java.util.ArrayList;
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Producto> lista = new ArrayList<>();
+		Producto aa = new Producto("01", "testArray");
+		lista.add(aa);
+		
+	}
+
+}
