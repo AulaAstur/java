2919f7243ffec3a92c411085f628d3042d2f5f3b
Aula Astur <AulaAstur@users.noreply.github.com>
2016-05-19 18:44:02 +0200

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,13 +1,41 @@
 package almacen;
-import java.util.ArrayList;
+
+import java.util.*;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Producto> lista = new ArrayList<>();
-		Producto aa = new Producto("01", "testArray");
-		lista.add(aa);
-		
+		Scanner teclado = new Scanner(System.in);
+		String referencia;
+		String nombre;
+		int cantidad;
+		int opcion;
+
+		do {
+			System.out.println("Seleccione la opción a realizar");
+			System.out.println("---------------------");
+			System.out.println("1) Añadir producto");
+			System.out.println("2) Eliminar producto");
+			System.out.println("3) Mostrar numero de productos");
+			System.out.println("99) Salir del programa");
+			opcion = Integer.parseInt(teclado.nextLine());
+
+			if (opcion == 1) {
+
+				System.out.println("\nInserte referencia");
+				referencia = teclado.nextLine();
+
+				System.out.println("\nInserte nombre");
+				nombre = teclado.nextLine();
+
+				Producto aa = new Producto(referencia, nombre);
+				lista.add(aa);
+
+			} // if
+
+		} while (opcion != 99);
 	}
 
 }
