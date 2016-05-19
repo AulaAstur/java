4a79833da301f24f8a47d4b9631e363592ba0e25
Aula Astur <AulaAstur@users.noreply.github.com>
2016-05-19 17:20:32 +0200

    Update Cambios.java

--- Cambios.java
+++ Cambios.java
@@ -1,5 +1,5 @@
 		/*
-		 * Crear un producto p1
+		 * Crear un producto p1 YAY
 		 * Y system out println cambiando atributos.
 		 * 
 		 * Comillas dobles string, simples char
@@ -14,11 +14,12 @@ public class Cambios {
 		System.out.println("Generación de un producto");
 		Producto p1 = new Producto("0","cero", 0);
 		
+		
+		System.out.println("Anterior cantidad: "+p1.getCantidad());
+		System.out.println("Anterior referencia: "+p1.getReferencia());
 		p1.setCantidad(1);
-		
-		System.out.println(p1.getCantidad());
-		
+		p1.setReferencia("0100");
+		System.out.println("\nCambios de datos efectuados:");
+		System.out.println("Nueva cantidad: "+p1.getCantidad());
+		System.out.println("Nueva referencia: "+p1.getReferencia());
 		}
-
-	}
-	
